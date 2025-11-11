55f625f4b127f18c203c0775fbec39dfa06c14ee
Sunil Dalai <sunildalai6128@gmail.com>
2025-11-11 18:30:35 +0530

    fix: Update allowed origins in CORS configuration to include additional frontend URL

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 const allowedOrigins = process.env.ORIGIN_URL
   ? process.env.ORIGIN_URL.split(',')
   //:['https://rent-x-frontend.vercel.app/'];
-  : ['http://localhost:5173']; // frontend React default port
+  : ['http://localhost:5173','https://rent-x2.vercel.app/']; // frontend React default port
 app.use(
   cors({
     origin: allowedOrigins,
